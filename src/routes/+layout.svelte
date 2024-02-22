157b75e7c9f087d781fbfdb20a9213c8a8ba169c
James Appleton <james.appleton@optusnet.com.au>
2024-02-22 14:25:34 +1100

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+    // tailwind
+    import "tailwindcss/tailwind.css";
+    const alignment = "w-full flex items-center justify-center";
+
+    // page components
+    import Navbar from "./Navbar.svelte";
+    import Footer from "./Footer.svelte";
+</script>
+
+<svelte:head>
+    <link rel="stylesheet" href="output.css" />
+</svelte:head>
+
+<div class="items-center justify-center min-h-screen flex-col flex">
+    <div class="w-full flex justify-center items-center bg-base-300">
+        <Navbar />
+    </div>
+
+    <div class="w-full max-w-screen-lg text-left flex-1 p-5">
+            <slot />
+    </div>
+
+    <div class="w-full flex">
+        <Footer />
+    </div>
+</div>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,16 @@
+<div class="navbar max-w-screen-lg justify-between p-3">
+    <div class="flex-1">
+        <a href="/" class="btn btn-ghost text-xl">James Appleton</a>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1 text-base">
+            <li><a href="/resume">Resume</a></li>
+            <li><a href="/academics">Academics</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <!-- <li><a href="/blog">Blog</a></li> -->
+        </ul>
+        <div >
+            <a href="/blog" class="btn bg-base-100 btn-ghost">Blog</a>
+        </div>
+    </div>
+</div>
