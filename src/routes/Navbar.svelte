38bfb9538ba9e6cda202fd1f53df506d0ee031c4
James Appleton <james.appleton@optusnet.com.au>
2024-02-23 21:58:53 +1100

    contact info complete, resume in progress

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -4,17 +4,17 @@
 
 <div class="navbar max-w-screen-lg justify-between p-3">
     <div class="flex-1">
-        <a href="/" class="btn btn-ghost text-xl">James Appleton</a>
+        <a href="{base}/" class="btn btn-ghost text-xl">James Appleton</a>
     </div>
     <div class="navbar-center hidden lg:flex">
         <ul class="menu menu-horizontal px-1 text-base">
-            <li><a href="{base}/resume">Resume</a></li>
-            <li><a href="{base}/academics">Academics</a></li>
-            <li><a href="{base}/contact">Contact</a></li>
+            <li><a href="{base}/resume/">Resume</a></li>
+            <li><a href="{base}/academics/">Academics</a></li>
+            <li><a href="{base}/contact/">Contact</a></li>
             <!-- <li><a href="/blog">Blog</a></li> -->
         </ul>
         <div >
-            <a href="{base}" class="btn bg-base-100 btn-ghost">Blog</a>
+            <a href="{base}/" class="btn bg-base-100 btn-ghost">Blog</a>
         </div>
     </div>
 </div>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,33 +1,48 @@
 <article class="prose-lg prose-zinc w-full">
     <h1>Resume</h1>
 
-    <div class="divider"/>
-        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-         Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero
-          sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
-
-
-    <div class="divider"/>
-    <h3><i>Work Experience</i></h3>
+    <h3>Career Profile</h3>
+    <div class="divider" />
     <p>
-        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero
-         sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+        ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+        egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend
+        leo.
     </p>
 
-    <div class="divider"/>
-    <h3><i>Work Experience</i></h3>
-    <p>
-        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero
-         sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
-    </p>
+    <h3>Education</h3>
+    <div class="divider" />
+    <div class="flex flex-row justify-between my-2">
+        <div>
+            <h4 >
+                <b>
+                    Bachelor of Mechanical Engineering (Honours) / Bachelor of
+                    Computer Science
+                    <br />University of New South Wales
+                </b>
+            </h4>
+        </div>
 
-    <div class="divider"/>
-    <h3><i>Work Experience</i></h3>
+        <div>
+            <h4 class="flex">2020 - Present</h4>
+        </div>
+    </div>
+    <div>
+        <ul class="list-disc my-1">
+            <li class="my-1">test</li>
+            <li class="my-1">test</li>
+            <li class="my-1">test</li>
+        </ul>
+    </div>
+
+    <h3>Employment</h3>
+    <div class="divider" />
     <p>
-        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero
-         sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+        ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+        egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend
+        leo.
     </p>
 </article>
